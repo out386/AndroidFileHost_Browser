00209308f35950ad2501e354a6f888ec1c917d35
Harsh Shandilya <msfjarvis@gmail.com>
2016-10-30 02:10:08 +0530

    Generify script(s)
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function welcome() {
-  echo "This is the configuration script to start building KPSConnect."
+  echo "This is the configuration script to start building AFH Browser."
   echo "Please make sure to have a private key created in order to proceed."
   echo "If you don't have one, create one using i. e. Android Studio"
   echo "Then come back to this script and enter your credentials"
@@ -14,26 +14,26 @@ function goodbye() {
 welcome
 
 if [ "$1" == "--clearPw" ]; then
-  export KPSCONNECT_KEY_PASSWD=""
-  export KPSCONNECT_KEYSTORE_PASSWD=""
+  export KEY_PASSWD=""
+  export KEYSTORE_PASSWD=""
 fi
 
-KPSCONNECT_KEYSTOREPWD=""
-KPSCONNECT_KEYPWD=""
+KEYSTOREPWD=""
+KEYPWD=""
 
 if [ "$1" == "--gpw" ]; then
   export KEY_PASSWD="$2"
   export KEYSTORE_PASSWD="$3"
 fi
 
-if [ -z $KPSCONNECT_KEY_PASSWD ]; then
+if [ -z $KEY_PASSWD ]; then
   echo "First of all the keystore needs to be configured."
   echo -en "\nKeystore password: "
-  read -s -p "" KPSCONNECT_KEYSTOREPWD
+  read -s -p "" KEYSTOREPWD
   echo -en "\nKey      password: "
-  read -s -p "" KPSCONNECT_KEYPWD
-  export KEY_PASSWD="$KPSCONNECT_KEYPWD"
-  export KEYSTORE_PASSWD="$KPSCONNECT_KEYSTOREPWD"
+  read -s -p "" KEYPWD
+  export KEY_PASSWD="$KEYPWD"
+  export KEYSTORE_PASSWD="$KEYSTOREPWD"
   echo -en "\n"
 fi
 
