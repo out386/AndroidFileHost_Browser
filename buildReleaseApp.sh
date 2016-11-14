97cadc187ba8b2a3504787d780f046db7b58ef03
Harsh Shandilya <msfjarvis@gmail.com>
2016-11-14 15:46:42 +0530

    Adjust build script for previous commit
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- buildReleaseApp.sh
+++ buildReleaseApp.sh
@@ -6,7 +6,7 @@ source configure.sh
 echo "Building..."
 ./gradlew clean assembleRelease
 
-[ -f app/app-release.apk ] || cp app/build/outputs/apk/app-release.apk app/app-release.apk
-[ -f app/app-release.apk ] && echo "APK Compiled" || exit 1
+[ -f app/app-release.apk ] || cp app/build/outputs/apk/afh-browser_beta*.apk app/
+[ -f app/afh-browser_beta*.apk ] && echo "APK Compiled" || exit 1
 
 echo "Build finished!"
