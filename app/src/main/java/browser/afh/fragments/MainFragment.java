e8a63150677f6047a2adf5ff1dc04421e7cf8974
Ritayan Chakraborty <ritayanout@gmail.com>
2016-11-15 10:14:42 +0530

    Use a singleton for Volley

--- app/src/main/java/browser/afh/fragments/MainFragment.java
+++ app/src/main/java/browser/afh/fragments/MainFragment.java
@@ -23,31 +23,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.android.volley.Cache;
-import com.android.volley.Network;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HurlStack;
-
 import browser.afh.data.FindDevices;
 import browser.afh.R;
+import browser.afh.tools.VolleySingleton;
 
 public class MainFragment extends Fragment {
     View rootView;
-    RequestQueue queue;
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.main_fragment, container, false);
 
-        final int NETWORK_THREAD_POOL_SIZE = 30;
-        Cache cache = new DiskBasedCache(getActivity().getCacheDir(), 1024 * 1024);
-        Network network = new BasicNetwork(new HurlStack());
-        queue = new RequestQueue(cache, network, NETWORK_THREAD_POOL_SIZE);
-
-        new FindDevices(rootView, queue).findFirstDevice();
+        new FindDevices(rootView, VolleySingleton.getInstance(getActivity()).getRequestQueue()).findFirstDevice();
         return rootView;
     }
 }
