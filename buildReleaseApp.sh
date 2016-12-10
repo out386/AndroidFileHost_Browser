04b8bb69512ad5886b934cb29c22b9afec87af85
Harsh Shandilya <msfjarvis@gmail.com>
2016-12-11 00:41:40 +0530

    Use native gradle binary if available

--- buildReleaseApp.sh
+++ buildReleaseApp.sh
@@ -4,6 +4,12 @@ echo "Configuring..."
 source configure.sh
 
 echo "Building..."
+if [ $(which gradle) != "" ];
+then
+gradle clean
+gradle :app:assembleRelease
+else
 ./gradlew clean assembleRelease
+fi
 
 [ $? -eq 0 ] || echo "Build Failed!"
