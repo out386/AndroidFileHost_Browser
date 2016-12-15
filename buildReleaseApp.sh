03afeb6d2c3ab8428f3e49938284031cd48cf92d
Ritayan Chakraborty <ritayanout@gmail.com>
2016-12-15 18:14:25 +0530

    Revert "Use native gradle binary if available"
    This reverts commit 04b8bb69512ad5886b934cb29c22b9afec87af85.

--- buildReleaseApp.sh
+++ buildReleaseApp.sh
@@ -4,12 +4,6 @@ echo "Configuring..."
 source configure.sh
 
 echo "Building..."
-if [ $(which gradle) != "" ];
-then
-gradle clean
-gradle :app:assembleRelease
-else
 ./gradlew clean assembleRelease
-fi
 
 [ $? -eq 0 ] || echo "Build Failed!"
