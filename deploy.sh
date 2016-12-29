621a3c3772e53b2aba1a458f2138749fc5c99e0d
Ritayan Chakraborty <ritayanout@gmail.com>
2016-12-29 13:24:18 +0530

    CircleCI: Make scripts more readable

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
-export version_name=v$(cat app/build.gradle | grep "versionName" | cut -d '"' -f2)
+#!/bin/bash
 
+# This script uploads the APK resulting from a CI build to Github Releases
+version_name=v$(cat app/build.gradle | grep "versionName" | cut -d '"' -f2)
+
+# github-release (https://github.com/aktau/github-release) is downloaded by the CI
+
+# Create a release for Github
 ./bin/linux/amd64/github-release release\
     --user out386 \
     --repo AndroidFileHost_Browser \
     --tag $version_name \
     --name $version_name \
 
+# Upload the APK to Github
 ./bin/linux/amd64/github-release upload \
     --user out386 \
     --repo AndroidFileHost_Browser \
@@ -13,7 +20,8 @@ export version_name=v$(cat app/build.gradle | grep "versionName" | cut -d '"' -f
     --name "AFH_Browser_$version_name.apk"  \
     --file app/build/outputs/apk/afh-browser_beta_*.apk
 
-export tg_message="A new release $version_name was just made for AFHBrowser. Will do this for commits later."
+tg_message="A new release $version_name was just made for AFHBrowser. Here's the (apk)[https://github.com/out386/AndroidFileHost_Browser/releases/download/$version_name/AFH_Browser_$version_name.apk]"
 
-curl "https://api.telegram.org/bot$tg_bot_key/sendmessage" --data "text=$tg_message&chat_id=$tg_out_id"
-curl "https://api.telegram.org/bot$tg_bot_key/sendmessage" --data "text=$tg_message&chat_id=$tg_msf_id"
+# Notify via Telegram
+curl -F chat_id="$tg_out_id" -F parse_mode="markdown" -F text="$tg_message" "https://api.telegram.org/bot$tg_bot_key/sendMessage"
+curl -F chat_id="$tg_msf_id" -F parse_mode="markdown" -F text="$tg_message" "https://api.telegram.org/bot$tg_bot_key/sendMessage"
--- test_deploy.sh
+++ test_deploy.sh
@@ -1,13 +1,19 @@
+#!/bin/bash
+
+# This script sends test APKs via a telegram bot. Useful for testing.
+
 version_name=v$(cat app/build.gradle | grep "versionName" | cut -d '"' -f2)
 version_code=$(cat app/build.gradle | grep -m 1 "versionCode" | awk  '{print $2}')
 apk="app/build/outputs/apk/afh-browser_beta_$version_code.apk"
 
 tg_message="New commits were pushed to the test branch of AFHBrowser.
-Commits are (here)[https://github.com/out386/AndroidFileHost_Browser/commits/tree].
+Commits are [here](https://github.com/out386/AndroidFileHost_Browser/commits/test).
 Here is the apk:"
 
-curl "https://api.telegram.org/bot$tg_bot_key/sendmessage" --data "text=$tg_message&chat_id=$tg_out_id"
-curl "https://api.telegram.org/bot$tg_bot_key/sendmessage" --data "text=$tg_message&chat_id=$tg_msf_id"
+# Notify via Telegram
+curl -F chat_id="$tg_out_id" -F parse_mode="markdown" -F text="$tg_message" "https://api.telegram.org/bot$tg_bot_key/sendMessage"
+curl -F chat_id="$tg_msf_id" -F parse_mode="markdown" -F text="$tg_message" "https://api.telegram.org/bot$tg_bot_key/sendMessage"
 
-curl -F chat_id=$tg_out_id -F document=@$apk "https://api.telegram.org/bot$tg_bot_key/senddocument"
-curl -F chat_id=$tg_msf_id -F document=@$apk "https://api.telegram.org/bot$tg_bot_id/senddocument"
+# Send the APK directly to the Telegram bot
+curl -F chat_id="$tg_out_id" -F document=@$apk "https://api.telegram.org/bot$tg_bot_key/sendDocument"
+curl -F chat_id="$tg_msf_id" -F document=@$apk "https://api.telegram.org/bot$tg_bot_id/sendDocument"
